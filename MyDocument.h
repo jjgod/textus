0b25e6f2956ed15d810bb3f4fc5be11942fdb958
Jjgod Jiang <gzjjgod@gmail.com>
2009-03-16 12:59:45 +0800

    Initial checkin, no functions

---
+++ MyDocument.h
@@ -0,0 +1,15 @@
+//
+//  MyDocument.h
+//  Textus
+//
+//  Created by Jjgod Jiang on 2/16/09.
+//  Copyright Jjgod Jiang 2009 . All rights reserved.
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSDocument
+{
+}
+@end
---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Textus
+//
+//  Created by Jjgod Jiang on 2/16/09.
+//  Copyright Jjgod Jiang 2009 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **) argv);
+}

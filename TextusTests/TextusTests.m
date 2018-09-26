f604ff8800dbf5ada42ee620ac7afc84d72ee2bc
Jiang Jiang <gzjjgod@gmail.com>
2018-09-26 10:35:35 -0700

    Add an empty test target

---
+++ TextusTests/TextusTests.m
@@ -0,0 +1,36 @@
+//
+//  TextusTests.m
+//  TextusTests
+//
+//  Created by Jiang Jiang on 9/26/18.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TextusTests : XCTestCase
+
+@end
+
+@implementation TextusTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
